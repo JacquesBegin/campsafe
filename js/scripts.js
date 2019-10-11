2a33912fa1d42a0341df12a228d3ac6862525a12
Jacques Begin <jacques.andre.begin@gmail.com>
2019-10-11 18:58:29 -0400

    Creates temporary variable to hold gampground data for testing purposes. Also suspends use of the campground fetch call until server is setup.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,24 @@
 document.getElementById("selectStateButton").addEventListener("click", stateSearch);
 
+let campground = [
+  {
+    contractID: "KOA",
+    contractType: "PRIVATE",
+    facilityID: "191502",
+    facilityName: "Alamosa KOA",
+    facilityPhoto: "/photos/image.jpg",
+    latitude: "37.4744444",
+    longitude: "-105.7986111",
+    sitesWithAmps: "Y",
+    sitesWithPetsAllowed: "Y",
+    sitesWithSewerHookup: "N",
+    sitesWithWaterHookup: "Y",
+    sitesWithWaterfront: "",
+    state: "CO"
+  }
+]
+
+
 function displayCopyrightYear() {
   let date = new Date;
   document.getElementById("footerYear").innerHTML = date.getFullYear();
@@ -8,6 +27,11 @@ displayCopyrightYear();
 
 function stateSearch() {
   let state = document.getElementById("selectStateList").value;
-  console.log(`Returned from fetch: ${fetchState(state)}`);
+  
+  // Use the following line to fetch the campground list once
+  // the server has been setup.
+  // console.log(`Returned from fetch: ${fetchState(state)}`);
+
+
 }
 
