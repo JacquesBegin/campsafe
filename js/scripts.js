833356dc52e25d97a76ab1542fd7e3913907e930
Jacques Begin <jacques.andre.begin@gmail.com>
2019-10-14 21:12:35 -0400

    Adds names of all 50 states and abbreviations.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 document.getElementById("selectStateButton").addEventListener("click", stateSearch);
 
-let campgrounds = [
+const campgrounds = [
   {
     contractID: "KOA",
     contractType: "PRIVATE",
@@ -68,12 +68,65 @@ let campgrounds = [
   }
 ];
 
-let alertTypes = [
+const alertTypes = [
   "Bear spotted in the park! Use proper food storage techniques.",
   "Heavy rain is forecasted for this area. Flooding could occur!",
   "Forest fire near campground! Camping is prohibited until further notice.",
   "Rock slide on park road 3A! Use alternate routes."
-]
+];
+
+const states = {
+  AK: "Alaska",
+  AL: "Alabama",
+  AZ: "Arizona",
+  AR: "Arkansas",
+  CA: "California",
+  CO: "Colorado",
+  CT: "Connecticut",
+  DE: "Delaware",
+  FL: "Florida",
+  GA: "Georgia",
+  HI: "Hawaii",
+  ID: "Idaho",
+  IL: "Illinois",
+  IN: "Indiana",
+  IA: "Iowa",
+  KS: "Kansas",
+  KY: "Kentucky",
+  LA: "Louisiana",
+  ME: "Maine",
+  MD: "Maryland",
+  MA: "Massachusetts",
+  MI: "Michigan",
+  MN: "Minnesota",
+  MS: "Mississippi",
+  MO: "Missouri",
+  MT: "Montana",
+  NE: "Nebraska",
+  NV: "Nevada",
+  NH: "New Hampshire",
+  NJ: "New Jersey",
+  NM: "New Mexico",
+  NY: "New York",
+  NC: "North Carolina",
+  ND: "North Dakota",
+  OH: "Ohio",
+  OK: "Oklahoma",
+  OR: "Oregon",
+  PA: "Pennsylvania",
+  RI: "Rhode Island",
+  SC: "South Carolina",
+  SD: "South Dakota",
+  TN: "Tennessee",
+  TX: "Texas",
+  UT: "Utah",
+  VT: "Vermont",
+  VA: "Virginia",
+  WA: "Washington",
+  WV: "West Virginia",
+  WI: "Wisconsin",
+  WY: "Wyoming"
+};
 
 
 
