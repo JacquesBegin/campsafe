d32093a07d2ebc3c980b03d3e599de0e8826b81f
Jacques Begin <jacques.andre.begin@gmail.com>
2019-10-17 22:13:50 -0400

    Adds user instructions.

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const port = 3333;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/plain");
-  res.end("Hello World\n");
+  res.send("Hello World\n");
 });
 
 server.listen(port, hostname, () => {
