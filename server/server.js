54016937ce57bf8f3652174a66c34098ad1f5763
Jacques Begin <jacques.andre.begin@gmail.com>
2019-10-31 23:44:23 -0400

    Adds dotenv and uses it to save port number.

--- server/server.js
+++ server/server.js
@@ -1,8 +1,9 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 
 const app = express();
-const PORT = 7000;
+const PORT = process.env.PORT;
 
 app.use(cors());
 
