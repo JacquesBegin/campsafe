a32d22cd4ec9f007ab53bf57a4ac0dbc899f80be
Jacques Begin <jacques.andre.begin@gmail.com>
2019-10-31 23:01:24 -0400

    Adds first endpoint to /hello.

--- server/server.js
+++ server/server.js
@@ -3,6 +3,11 @@ const express = require("express");
 const app = express();
 const PORT = 7000;
 
+app.get("/hello", (req,res) => {
+  res.send("Hello World");
+});
+
+// Starts the server to listen on 
 app.listen(PORT, () => {
   console.log(`Server is listening on port: ${PORT}`);
 });
