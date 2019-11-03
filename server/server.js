86a4d30d4a0211e38dfb97c5e5790fb0919f3026
Jacques Begin <jacques.andre.begin@gmail.com>
2019-11-02 23:47:51 -0400

    Uses absolute path for accessing index.html.

--- server/server.js
+++ server/server.js
@@ -10,12 +10,14 @@ app.use("/static", express.static("public"));
 
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
+  res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
 
 
-
+app.get("/campgrounds/", (req,res) => {
+  res.send("Campgrounds");
+});
 
 app.get("/campgrounds/:state", (req,res) => {
   res.send("Campgrounds");
