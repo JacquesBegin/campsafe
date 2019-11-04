0a26fb5d826b7a53505c6559258420ca34709077
Jacques Begin <jacques.andre.begin@gmail.com>
2019-11-03 22:57:42 -0500

    Removes campground routes from the main server file, adds import for campground router, and comment for route purpose.

--- server/server.js
+++ server/server.js
@@ -2,45 +2,22 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 
+// Required Route Files
+const campgrounds = require("./routes/campgroundSearch");
+
 const app = express();
 const PORT = process.env.PORT;
 
 app.use(cors());
 app.use("/static", express.static("public"));
+app.use("/campgrounds", campgrounds);
 
 
+// Default route to serve home page
 app.get("/", (req, res) => {
   res.sendFile("index.html", { root: __dirname + "/../" });
 });
 
-
-
-app.get("/campgrounds/", (req,res) => {
-  
-  res.send("Campgrounds");
-});
-
-
-// Make a call from the front end to this route to get all campgrounds
-// for the selected state.
-app.get("/campgrounds/:state", (req,res) => {
-  // TODO res.json()
-  res.send("Campgrounds");
-});
-
-
-
-// app.post("/", (req, res) => {
-//   res.send("Received POST HTTP method");
-// });
-
-// app.put("/", (req, res) => {
-//   res.send("Received PUT HTTP method");
-// });
-
-// app.delete("/", (req, res) => {
-//   res.send("Received DELETE HTTP method");
-// });
 
 // Starts the server to listen on 
 app.listen(PORT, () => {
