2f2854103e6045f16340aa5834ca5003851871b8
Jacques Begin <jacques.andre.begin@gmail.com>
2019-11-03 22:59:37 -0500

    Adds default route and route for getting campgrounds by state.

--- server/routes/campgroundSearch.js
+++ server/routes/campgroundSearch.js
@@ -4,3 +4,13 @@ const router = express.Router();
 // Possibly create a log file that tracks each time a route is called 
 // and what data is being searched for.
 
+router.get("/", (req, res) => {
+  res.send("Campground Route");
+});
+
+router.get("/campground/:state", (req, res) => {
+res.send("Campgrounds for a state");
+});
+
+
+module.exports = router;
