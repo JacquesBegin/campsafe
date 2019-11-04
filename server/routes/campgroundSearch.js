8a9dadb0950d120f0e490f7cbeea553f884eb41b
Jacques Begin <jacques.andre.begin@gmail.com>
2019-11-03 22:22:15 -0500

    Sets up the router for the file and adds comment for future feature (logging user interaction).

---
+++ server/routes/campgroundSearch.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const router = express.Router();
+
+// Possibly create a log file that tracks each time a route is called 
+// and what data is being searched for.
+
