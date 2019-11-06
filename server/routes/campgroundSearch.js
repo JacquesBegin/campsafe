86c39b9ffa70b16416af55bec88cb0b7c63749ec
Jacques Begin <jacques.andre.begin@gmail.com>
2019-11-05 22:48:19 -0500

    Adds require for node-fetch, corrects object typo, and creates initial URL for API call.

--- server/routes/campgroundSearch.js
+++ server/routes/campgroundSearch.js
@@ -1,34 +1,44 @@
 require("dotenv").config();
 const express = require("express");
 const router = express.Router();
+const fetch = require("node-fetch");
 const xml2json = require("xml2js")
 
 const apiKey = process.env.CAMP_API_KEY;
 
 let queryParams = {
-  landmarkLat = "",
-  landmarkLong = "",
-  siteType = "",
-  pstate = "",
-  pname = "",
-  amenity = "",
-  eqplen = "",
-  Maxpeople = "",
-  hookup = "",
-  water = "",
-  sewer = "",
-  pull = "",
-  pets = "",
-  waterfront = ""
+  landmarkLat: "",
+  landmarkLong: "",
+  siteType: "",
+  pstate: "",
+  pname: "",
+  amenity: "",
+  eqplen: "",
+  Maxpeople: "",
+  hookup: "",
+  water: "",
+  sewer: "",
+  pull: "",
+  pets: "",
+  waterfront: ""
 }
 
 
-// Possibly create a log file that tracks each time a route is called 
+// TODO Possibly create a log file that tracks each time a route is called 
 // and what data is being searched for.
 
 router.post("/", (req, res) => {
   // Use req.body to retrieve parameters to use in Active api URL
   console.log(`req.body1: ${JSON.stringify(req.body)}`);
+
+  let URL = `http://api.amp.active.com/camping/campgrounds?`;
+  let URL_api_key = `&api_key=${apiKey}`;
+
+  // Build query string from the req.body params
+  for ()
+
+  // Make call to Active API to retrieve campgrounds for a State
+  fetch("")
 
   // Convert from xml to json
 
