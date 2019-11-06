51e47ce3aaca3ef0e944199fe208495e1c0ee718
Jacques Begin <jacques.andre.begin@gmail.com>
2019-11-05 20:59:17 -0500

    Creates a global object variable for all available query parameters for campground search from Active API.

--- server/routes/campgroundSearch.js
+++ server/routes/campgroundSearch.js
@@ -5,6 +5,23 @@ const xml2json = require("xml2js")
 
 const apiKey = process.env.CAMP_API_KEY;
 
+let queryParams = {
+  landmarkLat = "",
+  landmarkLong = "",
+  siteType = "",
+  pstate = "",
+  pname = "",
+  amenity = "",
+  eqplen = "",
+  Maxpeople = "",
+  hookup = "",
+  water = "",
+  sewer = "",
+  pull = "",
+  pets = "",
+  waterfront = ""
+}
+
 
 // Possibly create a log file that tracks each time a route is called 
 // and what data is being searched for.
