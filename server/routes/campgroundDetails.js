4a6273168313ae2495d8ab3829e20488765e0d9e
Jacques Begin <jacques.andre.begin@gmail.com>
2019-11-10 10:09:45 -0500

    Adds initial URL for retrieving campground details.

--- server/routes/campgroundDetails.js
+++ server/routes/campgroundDetails.js
@@ -6,3 +6,8 @@ const fs = require("fs");
 const xml2json = require("xml2js").parseString;
 
 const apiKey = process.env.CAMP_API_KEY;
+
+// Initial URL for Active API
+let URL = `http://api.amp.active.com/camping/campground/details?`;
+// Add api_key to the end of the query string
+let URL_api_key = `api_key=${apiKey}`;
