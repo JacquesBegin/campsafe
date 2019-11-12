601a4b8e8b0128b058c4ab38b4cdaf945c25c33f
Jacques Begin <jacques.andre.begin@gmail.com>
2019-11-11 20:59:09 -0500

    Adds route for retrieving campground details (currently not returning proper data).

--- server/routes/campgroundDetails.js
+++ server/routes/campgroundDetails.js
@@ -10,12 +10,59 @@ const apiKey = process.env.CAMP_API_KEY;
 
 
 router.post("/", (req, res) => {
+
   let bodyParams = req.body;
 
-  // Initial URL for Active API
-  let URL = `http://api.amp.active.com/camping/campground/details?`;
-  // Add api_key to the end of the query string
-  let URL_api_key = `api_key=${apiKey}`;
+  // Constructs the URL for retrieving campground details
+  let URL = `http://api.amp.active.com/camping/campground/details?contractCode=${bodyParams.contractID}&parkId=${bodyParams.facilityID}&api_key=${apiKey}`;
+  console.log(URL);
+
+  fetch(URL, { method: "POST", redirect: "follow" })
+  .then(data => {
+    console.log("data", data);
+    return data.text();
+  })
+  .then(data => {
+    // Convert from xml to json
+    let tempResult;
+    xml2json(data, (err, result) => {
+      tempResult = result;
+    })
+    return tempResult;
+  })
+  .then(data => {
+    let jsonData = JSON.stringify(data);
+    // Create a file with response data
+    // if (bodyParams.pstate) {
+      fs.writeFile(`${__dirname}/../../dataFiles/${bodyParams.contractID}_${bodyParams.facilityID}.json`, jsonData, (err) => {
+        if (err) throw err;
+        console.log(`Saved file: ${bodyParams.contractID}_${bodyParams.facilityID}`);
+      })
+    // }
+    return jsonData;
+  })
+  .then(data => {
+    let dataObject = JSON.parse(data);
+    console.log(typeof dataObject);
+    console.log(dataObject);
+    // console.log(typeof dataObject.resultset.result);
+    // console.log(dataObject.resultset.result);
+    let dataArray = [];
+    // for (let element of dataObject.resultset.result) {
+    //   let siteObj = {
+    //     contractID: element["$"]["contractID"],
+    //     facilityID: element["$"]["facilityID"],
+    //     facilityName: element["$"]["facilityName"],
+    //     latitude: element["$"]["latitude"],
+    //     longitude: element["$"]["longitude"],
+    //     state: element["$"]["state"]
+    //   }
+    //   dataArray.push(siteObj);
+    // }
+    return dataArray;
+  })
+  .then(data => res.json({ data }))
+  .catch(err => console.error(err));
 });
 
 
